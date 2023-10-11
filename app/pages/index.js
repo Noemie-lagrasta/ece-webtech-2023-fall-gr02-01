3ea3a019e810c85db1e5bacabd3c10ea0c9b5c03
Noemie-Lagrasta <noemie.lagrasta@edu.ece.fr>
2023-10-11 09:56:44 +0200

    feat: add lab 4

---
+++ app/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from 'components/header'
+
+const Home = () => {
+  return (
+    <div>
+     <Header />
+      <main>
+        <h1>Welcome to Our Blog</h1>
+        <p>This is the home page of our blog.</p>
+      </main>
+    </div>
+  )
+}
+
+export default Home
