7cc4c0a104053b2b33445f643aab8f867b17d924
Noemie-Lagrasta <noemie.lagrasta@edu.ece.fr>
2023-10-11 09:56:44 +0200

    feat: add lab 4

---
+++ app/pages/contacts.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from 'components/header'
+
+const Contacts = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Contact Us</h1>
+        <p>You can reach out to us at example@example.com.</p>
+      </main>
+    </div>
+  )
+}
+
+export default Contacts
