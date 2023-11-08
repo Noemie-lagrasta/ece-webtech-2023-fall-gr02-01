2841b1382d1e6e3fbea534cec3d0f5e1e4f04080
Noemie-Lagrasta <noemie.lagrasta@edu.ece.fr>
2023-11-08 15:44:02 +0100

    feat: add lab 7

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,5 +1,8 @@
+import { ContextProvider } from '@/components/UserContext'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ContextProvider>
+    <Component {...pageProps} />
+  </ContextProvider>
 }
