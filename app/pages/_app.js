306658645c2573e91e24468ecfd81cfe9abdb607
aariane <ariane.aubrun@edu.ece.fr>
2023-11-17 18:50:52 +0100

    feat: add lab 8

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,8 +1,20 @@
-import { ContextProvider } from '@/components/UserContext'
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
 import '@/styles/globals.css'
+import { ContextProvider } from '../components/UserContext'
 
 export default function App({ Component, pageProps }) {
-  return <ContextProvider>
-    <Component {...pageProps} />
-  </ContextProvider>
+  // Create a new supabase browser client on every first render.
+  const [supabaseClient] = useState(() => createPagesBrowserClient())
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <ContextProvider>
+        <Component {...pageProps}/>
+      </ContextProvider>
+    </SessionContextProvider>
+  )
 }
